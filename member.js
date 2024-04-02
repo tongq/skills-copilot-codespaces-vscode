afc406083344e891cccdccc5b07957baf4fa5d68
Tong Quach <guojinshon@gmail.com>
2024-04-02 14:01:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember() {
+  const skills = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Node.js",
+    "MongoDB",
+    "Python",
+    "Django",
+    "Flask",
+  ];
+
+  return skills;
+}
+
+module.exports = skillsMember;
